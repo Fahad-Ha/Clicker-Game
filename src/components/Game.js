aa6859650d31ac7cb1cc49df9754e21928cd839b
Fahad <fahhod.ahmad@gmail.com>
2023-06-01 19:03:21 +0300

    V 1.0

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,37 +2,102 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 
 const Game = () => {
-  const [currPerSec, setcurrPerSec] = useState(0);
-  const [buidlingPrice, setBuildingPrice] = useState(20);
-  const [score, setScore] = useState(0);
-  const [currency, setCurrency] = useState(0);
-  const [buildings, setBuildings] = useState(0);
-  const [clickScore, setClickScore] = useState(1);
+  // const [currPerSec, setcurrPerSec] = useState(1);
+  // const [buidlingPriceForSec, setBuildingPriceForSec] = useState(20);
+  // const [buidlingPriceForClick, setBuildingPriceForClick] = useState(20);
+  // const [score, setScore] = useState(0);
+  // const [currency, setCurrency] = useState(0);
+  // const [buildings, setBuildings] = useState(0);
+  // const [clickScore, setClickScore] = useState(1);
+  // const [intervil, setIntervil] = useState(0);
+
+  const [currentValue, setCurrentValue] = useState({
+    currentPerSec: 1,
+    buidlingPriceForSec: 20,
+    buidlingPriceForClick: 20,
+    score: 0,
+    currency: 0,
+    buildings: 0,
+    clickScore: 1,
+    intervil: 0,
+  });
+
   function add() {
-    setScore(score + clickScore);
-    setCurrency(currency + clickScore);
+    setCurrentValue((prev) => ({
+      ...prev,
+      score: prev.score + prev.clickScore,
+      currency: prev.currency + prev.clickScore,
+    }));
+    // setScore(score + clickScore);
+    // setCurrency(currency + clickScore);
   }
   function getBuildingForSec() {
-    setCurrency(currency - buidlingPrice);
-    setBuildings(buildings + 1);
-    setBuildingPrice(buidlingPrice * 2);
-    setcurrPerSec(currPerSec + 1);
-    clickPerSec();
+    setCurrentValue(
+      (prev) => ({
+        ...prev,
+        currency: prev.currency - prev.buidlingPriceForSec,
+        buildings: prev.buildings + 1,
+        buidlingPriceForSec: prev.buidlingPriceForSec * 2,
+        intervil: clickPerSec(prev.intervil != 0 ? prev.intervil : 0),
+      })
+      // setCurrency(currency - buidlingPriceForSec);
+      // setBuildings(buildings + 1);
+      // setBuildingPriceForSec(buidlingPriceForSec * 2);
+      // setIntervil(clickPerSec(intervil != 0 ? intervil : ""));
+    );
   }
 
   function getBuildingForClick() {
-    setClickScore(clickScore * 2);
-    setCurrency(currency - buidlingPrice);
-    setBuildings(buildings + 1);
-    setBuildingPrice(buidlingPrice * 2);
+    setCurrentValue(
+      (prev) => ({
+        ...prev,
+        clickScore: prev.clickScore * 2,
+        currency: prev.currency - prev.buidlingPriceForClick,
+        buildings: prev.buildings + 1,
+        buidlingPriceForClick: prev.buidlingPriceForClick * 2,
+      })
+      // setClickScore(clickScore * 2);
+      // setCurrency(currency - buidlingPriceForClick);
+      // setBuildings(buildings + 1);
+      // setBuildingPriceForClick(buidlingPriceForClick * 2);
+    );
   }
+  function clickPerSec(intervil) {
+    clearInterval(intervil);
+    setCurrentValue((prev) => ({ ...prev, currPerSec: prev.currPerSec * 2 }));
 
-  function clickPerSec() {
-    setInterval(() => {
-      setCurrency((currency) => currency + 1);
-      setScore((score) => score + 1);
+    const intervil_ = setInterval(() => {
+      setCurrentValue((prev) => ({
+        ...prev,
+        currency: prev.currency + prev.currPerSec,
+        score: prev.score + prev.currPerSec,
+      }));
+      console.log(currentValue.currPerSec);
+      // setCurrency((currency) => currency + currPerSec);
+      // setScore((score) => score + currPerSec);
+      // console.log(currPerSec);
     }, 1000);
+    return intervil_;
   }
+  // let clickPerSec = () => {};
+
+  // Before ALI UPDATE
+  // useEffect(() => {
+  //   let id = 0;
+  //   if (currPerSec != 0) {
+  //     id = setInterval(() => {
+  //       setCurrency((currency) => currency + currPerSec);
+  //       setScore((score) => score + currPerSec);
+  //       console.log(`adding ${currPerSec}`);
+  //     }, 1000);
+  //   }
+
+  //   console.log("Added another one");
+  //   return () => clearInterval(id);
+  //   // return () =>
+  // }, [buildings]);
+
+  // END
   //   useEffect(() => {
   //     clickPerSec();
   //     console.log("test");
@@ -40,23 +105,25 @@ const Game = () => {
 
   return (
     <div className="card">
-      <h3>{`Buildings: ${buildings}`}</h3>
-      <h4>{`${currency} KD`}</h4>
-      <h1>{score}</h1>
-      <p>{`Score per click: ${clickScore}`}</p>
-      <p>{`Currency per second: ${currPerSec}`}</p>
+      <h3>{`Buildings: ${currentValue.buildings}`}</h3>
+      <h4>{`${currentValue.currency} KD`}</h4>
+      <h1>{currentValue.score}</h1>
+      <p>{`Score per click: ${currentValue.clickScore}`}</p>
+      <p>{`Currency per second: ${currentValue.currPerSec}`}</p>
       <button onClick={add}>CLick Me!</button>
-      {currency >= buidlingPrice ? (
-        <>
-          <button
-            className="btn-2"
-            onClick={getBuildingForSec}
-          >{`Buy a building +click per second `}</button>
-          <button
-            className="btn-3"
-            onClick={getBuildingForClick}
-          >{`Buy a building +currency per click `}</button>
-        </>
+      {currentValue.currency >= currentValue.buidlingPriceForSec ? (
+        <button
+          className="btn-2"
+          onClick={getBuildingForSec}
+        >{`Buy a building +click per second for ${currentValue.buidlingPriceForSec}`}</button>
+      ) : (
+        ""
+      )}
+      {currentValue.currency >= currentValue.buidlingPriceForClick ? (
+        <button
+          className="btn-3"
+          onClick={getBuildingForClick}
+        >{`Buy a building +currency per click for ${currentValue.buidlingPriceForClick}`}</button>
       ) : (
         ""
       )}
